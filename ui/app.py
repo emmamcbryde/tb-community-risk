e165dfe430f3372467bdbb541d642a3435b6ae9f
Emma McBryde <emma.mcbryde@jcu.edu.au>
2025-12-07 14:26:37 +0000

    Update app.py
    set path to main folder

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import os
+import sys
 
+# Ensure the project root is in sys.path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+# Now import your modules
 from ui.dynamic_ui import render_dynamic_ui
 from ui.static_ui import render_static_ui
+
 
 st.title("🧮 TB Community Risk Models")
 
