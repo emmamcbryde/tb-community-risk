655be771b643d83d5906ee10485e29a45cdf1381
Emma McBryde <emma.mcbryde@jcu.edu.au>
2026-02-16 15:31:36 +1100

    Delete dynamic_params.py
    remove and define the params directly from teh csv files

--- engine/dynamic/dynamic_params.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-def load_dynamic_parameters(directory="data/params_dynamic"):
-    params = {}
-    for f in Path(directory).glob("*.csv"):
-        params[f.stem] = pd.read_csv(f)
-    return params
